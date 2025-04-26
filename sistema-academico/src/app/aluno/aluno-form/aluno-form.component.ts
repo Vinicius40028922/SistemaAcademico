dd431394b9aee45bfd0e383b4d417acc00901c77
unknown <magalhaesv335@gmail.com>
2025-04-25 21:22:54 -0300

    trabalho inicial

---
+++ sistema-academico/src/app/aluno/aluno-form/aluno-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Aluno } from '../aluno.model';
+import { AlunoService } from '../aluno.service';
+
+@Component({
+ selector: 'app-aluno-form',
+ standalone: true,
+ imports: [CommonModule, FormsModule],
+ templateUrl: './aluno-form.component.html',
+ styleUrl: './aluno-form.component.css'
+})
+export class AlunoFormComponent {
+ nome: string = '';
+ nota: number = 0;
+ constructor(private alunoService: AlunoService) {}
+ adicionar() {
+const novoAluno = new Aluno(this.nome, this.nota);
+this.alunoService.adicionar(novoAluno);
+this.nome = '';
+this.nota = 0;
+ }
+}
